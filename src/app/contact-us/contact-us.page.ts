66209a18771bcbf514d15a9f7fe55a0c1b742785
ivica5859 <92518460+HuyenLuu88@users.noreply.github.com>
2021-12-15 10:26:20 -0300

    first commit

---
+++ src/app/contact-us/contact-us.page.ts
@@ -0,0 +1,144 @@
+import {Component, OnInit, ViewChild, ElementRef, NgZone, AfterContentInit, AfterViewInit} from '@angular/core';
+import {SharedDataService} from 'src/providers/shared-data/shared-data.service';
+import {ConfigService} from 'src/providers/config/config.service';
+import {NavController} from '@ionic/angular';
+import {LoadingService} from 'src/providers/loading/loading.service';
+import {HttpClient} from '@angular/common/http';
+import {Geolocation} from '@ionic-native/geolocation/ngx';
+import {google} from 'google-maps';
+
+declare var google: any;
+
+@Component({
+    selector: 'app-contact-us',
+    templateUrl: './contact-us.page.html',
+    styleUrls: ['./contact-us.page.scss'],
+})
+
+export class ContactUsPage implements OnInit {
+
+    @ViewChild('map', {static: false}) mapElement:ElementRef;
+    map: any;
+    //
+    contact = {
+        name: '',
+        email: '',
+        message: ''
+    };
+    errorMessage = '';
+
+    constructor(
+        public http: HttpClient,
+        public config: ConfigService,
+        public loading: LoadingService,
+        public shared: SharedDataService,
+        // public zone: NgZone,
+        private geolocation: Geolocation
+    ) {
+
+    }
+
+    // <!-- 2.0 updates -->
+    submit() {
+        this.loading.autoHide(2000);
+        var data = {};
+        data = this.contact;
+        this.http.get(this.config.url + '/api/appusers/send_mail/?insecure=cool&email=' + this.contact.email + '&name=' + this.contact.name + '&message=' + this.contact.message).subscribe((data: any) => {
+            console.log(data);
+
+            this.contact.name = '';
+            this.contact.email = '';
+            this.contact.message = '';
+            this.errorMessage = data;
+        }, error => {
+            this.errorMessage = JSON.parse(error._body).error;
+            console.log(this.errorMessage);
+        });
+
+
+    };
+
+    loadMap() {
+
+        // let coords = new google.maps.LatLng(24.774265, 46.738586);
+        // let mapOptions: google.maps.MapOptions = {
+        //     center: coords,
+        //     zoom: 14,
+        //     mapTypeId: google.maps.MapTypeId.ROADMAP
+        // };
+        //
+        // this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+        // let marker : google.maps.Marker = new google.maps.Marker({
+        //     map: this.map,
+        //     animation: google.maps.Animation.DROP,
+        //     position: coords
+        // });
+        // google.maps.event.addListener(marker, 'click', () => {
+        //     infoWindow.open(map, marker);
+        // });
+        let coords = new google.maps.LatLng(24.746441, 46.64289);
+        this.map = new google.maps.Map(this.mapElement.nativeElement, {
+            center: {lat: 24.746441, lng: 46.64289},
+            zoom: 18
+        });
+        const myApiKey = this.config.googleMapId;
+        const lat = parseFloat(this.config.latitude);
+        const lng = parseFloat(this.config.longitude);
+        let content = this.config.address;
+        const parentElement = this.mapElement.nativeElement;
+        const script = document.createElement('script');
+
+        let marker : google.maps.Marker = new google.maps.Marker({
+            map: this.map,
+            animation: google.maps.Animation.DROP,
+            position: coords
+        });
+
+        let infoWindow = new google.maps.InfoWindow({
+            content: content
+        });
+        google.maps.event.addListener(marker, 'click', () => {
+            infoWindow.open(this.map, marker);
+        });
+
+        try {
+          // script.src = "https://maps.googleapis.com/maps/api/js?key=" + myApiKey;
+          script.async = true;
+          script.defer = true;
+          script.onload = function () {
+
+            let map = new google.maps.Map(parentElement, {
+              center: { lat: 24.774265, lng: 46.738586 },
+              zoom: 15,
+              mapTypeId: google.maps.MapTypeId.ROADMAP
+            });
+
+            let marker = new google.maps.Marker({
+              map: map,
+              animation: google.maps.Animation.DROP,
+              position: map.getCenter()
+            });
+
+            let infoWindow = new google.maps.InfoWindow({
+              content: content
+            });
+
+            google.maps.event.addListener(marker, 'click', () => {
+              infoWindow.open(map, marker);
+            });
+          };
+          this.mapElement.nativeElement.insertBefore(script, null);
+        } catch (error) {
+
+        }
+    }
+
+    ngOnInit() {
+        // this.loadMap();
+        setTimeout(() => {
+            // console.log(this.mapElement.nativeElement);
+            this.loadMap()
+        }, 3000);
+    }
+
+}
---
+++ src/pipes/curency.pipe.ts
@@ -0,0 +1,36 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import * as $ from "jquery";
+@Pipe({
+  name: 'curency'
+})
+export class CurencyPipe implements PipeTransform {
+
+  // currency = $('<textarea />').html(localStorage.currency).text();
+  // currency = $('<textarea />').html("ر.س").text();
+  currency ="ر.س  ";
+  // currency ="SAR ";
+
+  position = localStorage.currencyPos;
+  decimal = localStorage.decimals;
+  constructor() {
+  }
+
+  transform(value) {
+
+    let v = parseFloat(value).toFixed(this.decimal);
+    if (v.toString() == 'NaN') {
+
+      if (this.position == 'left')
+        return this.currency + "" + value;
+      else
+        return value + " " + this.currency;
+    }
+    else {
+      if (this.position == 'left')
+        return this.currency + "" + v;
+      else
+        return v + " " + this.currency;
+    }
+  }
+
+}
